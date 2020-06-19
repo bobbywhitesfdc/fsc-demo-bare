ebf6fcbba2bb7d8764c2e8f21f64cbdd408c387d
Bobby White <bobby.white@salesforce.com>
2020-06-19 13:54:07 -0400

    Updated script, removing unneeded steps

--- orgInit.sh
+++ orgInit.sh
@@ -4,26 +4,13 @@ set -euxo pipefail
 #create scratch org
 sfdx force:org:create -f config/project-scratch-def.json -s -a FSCDEMO 
 
-#pckg installs
+#package installs of the main FSC package
+# Working around deprecation issues by allowing hand editing
+# See ./packages/installedPackage/FinServ.installedPackage
 sfdx force:mdapi:deploy -d packages -w 20 
 
-#sfdx force:package:install --package 04t1E000000cmtN -w 20
-
 sfdx force:package:install --package 04t1E000001Iql5 -w 20
-
-sfdx force:mdapi:deploy --deploydir mdapi-source/app-config
-
-sfdx force:mdapi:deploy --deploydir mdapi-source/data-config
-
-sfdx force:mdapi:deploy --deploydir mdapi-source/org-config
-
-sfdx force:source:push 
-
 sfdx force:user:permset:assign -n FinancialServicesCloudStandard
-
-sfdx force:apex:execute -f config/create-demo-data-setup.apex
-sfdx force:apex:execute -f config/create-demo-data.apex
-
 
 sfdx force:org:open
 
