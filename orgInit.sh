a05348382dde24186becc87585163f7ed7051b27
Bobby White <bobby.white@salesforce.com>
2025-11-18 11:36:30 -0500

    Converted to FSC on Core

--- orgInit.sh
+++ orgInit.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 # Exit on error!
 set -euxo pipefail
+
+function prop {
+    grep "${1}" project.properties|cut -d'=' -f2
+}
 #create scratch org
-sfdx force:org:create -f config/project-scratch-def.json -s -a FSCDEMO username=admin.bolt3@fsc.demo.org --durationdays 28
+# let it auto-assign the username ( username="$(prop 'user.admin' )" )
+#sf org create scratch -f config/project-scratch-def.json -a "$(prop 'default.env' )"  --set-default --duration-days 28
 
-#package installs of the main FSC package
-# Working around deprecation issues by allowing hand editing
-# See ./packages/installedPackage/FinServ.installedPackage
-sfdx force:mdapi:deploy -d packages -w 20 
-
-sfdx force:package:install --package 04t1E000001Iql5 -w 20
-sfdx force:user:permset:assign -n FinancialServicesCloudStandard
+sf org assign permsetlicense -n FinServ_FinancialServicesCloudStandardPsl
+sf org assign permsetlicense -n FSCInsurancePsl
 
 # Limited customization required for data loading
-sfdx force:source:push -f
-sfdx force:user:permset:assign -n FSC_Data_Load
-sfdx force:user:permset:assign -n Platform_Encryption_Management
+sf project deploy start -w 20
+#sf org assign permset -n FSC_Data_Load
 
-sfdx force:apex:execute -f scripts/apex/fixroles.apex
+#sfdx force:apex:execute -f scripts/apex/fixroles.apex
 
 #Populate some Test data
-sfdx force:data:bulk:upsert -u FSCDEMO -f data/households.csv -s Account -i Enterprise_Party_Id__c -w 20
-sfdx force:data:bulk:upsert -u FSCDEMO -f data/persons.csv -s Account -i Enterprise_Party_Id__c -w 20
-sfdx force:data:bulk:upsert -u FSCDEMO -f data/businesses.csv -s Account -i Enterprise_Party_Id__c -w 20
-sfdx force:data:bulk:upsert -u FSCDEMO -f data/acr.csv -s AccountContactRelation -i Enterprise_Party_Id__c -w 20
-sfdx force:data:bulk:upsert -u FSCDEMO -f data/aar.csv -s FinServ__AccountAccountRelation__c -i FinServ__ExternalId__c -w 20
+sf force data bulk upsert -o FSCDEMO -f data/households.csv -s Account -i Enterprise_Party_Id__c -w 20
+sf force data bulk upsert -o FSCDEMO -f data/persons.csv -s Account -i Enterprise_Party_Id__c -w 20
+sf force data bulk upsert -o FSCDEMO -f data/businesses.csv -s Account -i Enterprise_Party_Id__c -w 20
+sf force data bulk upsert -o FSCDEMO -f data/acr.csv -s AccountContactRelation -i Enterprise_Party_Id__c -w 20
+sf force data bulk upsert -o FSCDEMO -f data/aar.csv -s AccountRelationship -i AA_External_Id__c -w 20
+sf force data bulk upsert -o FSCDEMO -f data/aar.csv -s AccountRelationship -i AA_External_Id__c -w 20
+sf force data bulk upsert -o FSCDEMO -f data/policy.csv -s InsurancePolicy -i PolicyExtId__c -w 20
 
-sfdx force:data:bulk:upsert -u FSCDEMO -f data/financialaccounts.csv -s FinServ__FinancialAccount__c -i FinServ__FinancialAccountNumber__c -w 20
+#sf force data bulk upsert -o FSCDEMO -f data/financialaccounts.csv -s FinServ__FinancialAccount__c -i FinServ__FinancialAccountNumber__c -w 20
 
-sfdx force:org:open
+sf org open
