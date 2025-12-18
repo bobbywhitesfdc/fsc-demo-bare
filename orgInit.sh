9316ff12ac0a02682411a93a4a7fd761ed928d5f
Bobby White <bobby.white@salesforce.com>
2025-12-17 22:18:51 -0500

    checkpoint 2025-12-17 22:18:51

--- orgInit.sh
+++ orgInit.sh
@@ -7,14 +7,14 @@ function prop {
 }
 #create scratch org
 # let it auto-assign the username ( username="$(prop 'user.admin' )" )
-sf org create scratch -f config/project-scratch-def.json -a "$(prop 'default.env.alias' )"  --set-default --duration-days 28
+sf org create scratch -f config/project-scratch-def.json -a "$(prop 'default.env.alias' )" --username="$(prop 'user.admin' )" --set-default --duration-days 28
 
 sf org assign permsetlicense -n FinServ_FinancialServicesCloudStandardPsl
 sf org assign permsetlicense -n FSCInsurancePsl
-sf org assign permset -n FSC_Data_Load -i
+sf org assign permset -n FSC_Data_Load
 
 # Limited customization required for data loading
-sf project deploy start -w 20
+#sf project deploy start -w 20
 
 #sfdx force:apex:execute -f scripts/apex/fixroles.apex
 
